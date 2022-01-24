72b9d8cc44cdf96c7b5f672a9105596f059c1f42
carams <carams2020@gmail.com>
2022-01-24 12:04:44 +0300

    intermediate commit

--- SBot003/SBot003/DTO/UserInput.cs
+++ SBot003/SBot003/DTO/UserInput.cs
@@ -8,8 +8,11 @@ namespace SBot003.DTO
 {
     public class UserInput
     {
+        // Property contain input of User
         public string strMessage { get; set;}
+        // Handling input of User which using by Dispatcher
         public byte numChoice { get; set;}
+        // 
         public bool isParsed { get; set;} = false;
 
     }

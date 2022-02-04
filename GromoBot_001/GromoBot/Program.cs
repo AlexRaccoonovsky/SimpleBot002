cf7e92111e06046d4907093433ad98d064266be6
carams <carams2020@gmail.com>
2022-02-04 23:28:17 +0300

    Vew Module started

---
+++ GromoBot_001/GromoBot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using GromoBot.View
+    ;
+
+namespace GromoBot
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ GromoBot_001/GromoBot/View/GromoBotIO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GromoBot.View
+{
+    public class GromoBotIO
+    {
+        MainMenu mainMenu;
+
+        void ToInitEnvironment()
+        {
+            mainMenu = new MainMenu();
+        }
+    }
+}

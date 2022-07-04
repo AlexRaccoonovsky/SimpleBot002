5bebdd1a49bd9e84fcff07ee42f9238a8476e65a
carams <carams2020@gmail.com>
2022-07-05 00:02:28 +0300

    Cusor adding in a process

---
+++ GromoBot2/GromoBot2/IO/Cursor/Cursor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GromoBot2.IO.Cursor;
+
+namespace GromoBot2.IO.Cursor
+{
+    internal class Cursor
+    {
+        CursorPosition currentPosition;
+        public Cursor()
+        {
+            currentPosition = new CursorPosition(); 
+        }
+    }
+}
---
+++ GromoBot2/GromoBot2/IO/Cursor/CursorPosition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GromoBot2.IO.Cursor
+{
+    internal class CursorPosition
+    {
+        int numOfColumn { get; set; }
+        int numOfRow { get; set; }
+        public CursorPosition() 
+        { 
+            numOfColumn = 0;
+            numOfRow = 0;
+        }
+        public CursorPosition(int column, int row)
+        {   
+            numOfColumn = column;
+            numOfRow = row;
+        }
+    }
+}

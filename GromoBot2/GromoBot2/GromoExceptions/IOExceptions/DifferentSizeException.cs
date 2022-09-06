b0d2fb72ccced43f365759dd862f3696af7e5031
carams <carams2020@gmail.com>
2022-09-06 21:08:34 +0300

    Facing is present

---
+++ GromoBot2/GromoBot2/GromoExceptions/IOExceptions/DifferentSizeException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GromoBot2.GromoExceptions.IOExceptions
+{
+    public class DifferentSizeException : ApplicationException
+    {
+        public DateTime ErrorTimeStamp { get; set; }
+        public string CauseOfError { get; set; }
+        public DifferentSizeException()
+        { 
+            ErrorTimeStamp = DateTime.Now;
+            CauseOfError = string.Empty;
+        }
+        public DifferentSizeException(string message,string cause, DateTime time):base(message)
+        {
+            CauseOfError = cause;
+            ErrorTimeStamp = time;
+        }
+    }
+}

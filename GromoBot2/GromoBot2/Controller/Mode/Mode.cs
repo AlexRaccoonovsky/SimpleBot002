0362783574bbb139d8bde8107b20a7708088ee06
carams <carams2020@gmail.com>
2023-02-17 20:15:36 +0400

    Refactoring2023_2

---
+++ GromoBot2/GromoBot2/Controller/Mode/Mode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GromoBot2.IO;
+
+namespace GromoBot2.Controller.Mode
+{
+    public abstract class Mode
+    {
+        //public abstract GromoBotIO gromoBotIO { get; set; }
+        public abstract string Name { get; }
+       // public abstract StateOfGromo stateOfGromo { get; set; }
+        public abstract void ToStart(GromoBot bot);
+        //public abstract void ToInitializeEnvironment(GromoBotIO gromoIO, StateOfGromo currentGromoState);
+    }
+}
